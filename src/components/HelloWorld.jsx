9e0b3e9edcb65614d3fa0a0035319ce5aace2d45
abraham <abraalvare@gmail.com>
2020-08-19 23:40:32 -0600

    Eslint configurado

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const HelloWorld = ()=>(
+    <h1>Te Amo Dayani</h1>
+)
+
+export default HelloWorld
---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+
+
+/* reactDom recibe (componente,hacia donde se empuja el componente
+    ) */
+ReactDOM.render(<HelloWorld/>,document.getElementById('app'))
+
+
+
+
+
